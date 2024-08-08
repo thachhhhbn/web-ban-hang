56a2b6f4946c06fb2285bce9e968dc7870684189
Iambnthach <iambnthach@gmail.com>
2024-08-08 17:12:19 +0700

    feat -pagination

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -1,17 +1,41 @@
 const Product = require("./../../model/product.model");
 module.exports.index = async (req, res) => {
-  const find = {
+  //search
+  const find = {};
+  let keyword = "";
+  if (req.query.keyword) {
+    keyword = req.query.keyword;
+    const regex = new RegExp(keyword, "i");
+    find.title = regex;
+  }
+  //end search
+
+  //pagination
+
+  const objectPagination = {
+    currentPage: 1,
+    limitItem: 7,
   };
 
-  if (req.query.keyword) {
-    find.title = req.query.keyword;
+  if (req.query.page) {
+    objectPagination.currentPage = parseInt(req.query.page);
+    objectPagination.skip =
+      (objectPagination.currentPage - 1) * objectPagination.limitItem;
   }
 
-  const product = await Product.find(find);
+  const buttonsPagination = Math.ceil(
+   await Product.find().countDocuments() / objectPagination.limitItem
+  );
+
+  //end pagination
+  const product = await Product.find(find)
+    .limit(objectPagination.limitItem)
+    .skip(objectPagination.skip);
 
   res.render("../views/admin/pages/products/index.pug", {
-    
     title: "LIST PRODUCTS",
     list: product,
+    valueInput: find.title,
+    btnList:buttonsPagination
   });
 };
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,18 +1,30 @@
 // form search
 const formSearch = document.querySelector("#form-search");
 if (formSearch) {
-
   let url = new URL(window.location.href);
 
   formSearch.addEventListener("submit", (e) => {
     e.preventDefault();
     const keyword = e.target.elements.keyword.value;
-    if(keyword){
-        url.searchParams.set("keyword",keyword);
-    }else{
-        url.searchParams.delete("keyword",keyword);
+    if (keyword) {
+      url.searchParams.set("keyword", keyword);
+    } else {
+      url.searchParams.delete("keyword", keyword);
     }
-    window.location.href=url.href;
+    window.location.href = url.href;
   });
 }
 // end form search
+
+// btnPagination
+let url = new URL(window.location.href);
+var buttonsPagination = document.querySelectorAll(".page-link");
+buttonsPagination.forEach((button) => {
+  button.addEventListener("click", () => {
+    const page = button.getAttribute("btn-pagination");
+    url.searchParams.set("page", page);
+    window.location.href = url.href;
+  });
+});
+
+// end btnPagination
